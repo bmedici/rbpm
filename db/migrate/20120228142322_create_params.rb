a46fb095f6515569ae9df4a3a03bd89f0c81915f
Bruno <bmgit@bmconseil.com>
2012-03-26 02:35:14 +0200

    various changes

--- db/migrate/20120228142322_create_params.rb
+++ db/migrate/20120228142322_create_params.rb
@@ -8,6 +8,6 @@ class CreateParams < ActiveRecord::Migration
       t.timestamps
     end
     add_index :params, :step_id
-    add_index :params, :paramdef_id
+    add_index :params, :name
   end
 end
