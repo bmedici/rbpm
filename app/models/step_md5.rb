ba2354d6f2efcf4575f6198d8dc2786fbdaf0f8b
Bruno <bmgit@bmconseil.com>
2012-04-10 12:18:09 +0200

    various changes

--- app/models/step_md5.rb
+++ app/models/step_md5.rb
@@ -2,8 +2,8 @@ class StepMd5 < Step
 
   def paramdef
     {
-    :path_variable => { :description => "Variable containing path to the file", :format => :text, :lines => 1 },
-    :result_variable => { :description => "Variable receiving the result", :format => :text, :lines => 1 },
+    :file_to_hash => { :description => "Variable containing path to the file *", :format => :text, :lines => 2 },
+    :variable_to_set => { :description => "Variable receiving the result", :format => :text, :lines => 2 },
     }
   end
 
@@ -12,27 +12,31 @@ class StepMd5 < Step
   
   def run(current_job, current_action)
     # Init
-    path_variable = self.pval(:path_variable)
-    path_variable = self.pval(:result_variable)
+    file_to_hash = self.pval(:file_to_hash)
+    variable_to_set = self.pval(:variable_to_set)
     
     # Check for run context
     log "StepMd5 starting"
     return 21, "depends on the run context to gather variables, no valid current_job given" if current_job.nil?
 
+    # Evaluate source file and targt dir
+    evaluated_file_to_hash = current_job.evaluate(file_to_hash)
+    log "evaluated file_to_hash: #{evaluated_file_to_hash}"
+
     # Get the variable containing the path to hash
     log "variable containing path is #{path_variable}"
     filename_to_hash = current_job.get_var(path_variable)
-    return 22, "can't fetch the value of this variable" if filename_to_hash.nil?
+    return 22, "can't fetch the value of this variable" if evaluated_file_to_hash.nil?
 
-    log "this variable points to #{filename_to_hash}"
-    return 23, "can't find the file to hash in the filesystem" unless File.exists? filename_to_hash
+    log "this variable points to #{evaluated_file_to_hash}"
+    return 23, "can't find the file to hash in the filesystem" unless File.exists? evaluated_file_to_hash
 
     # Hash this filepath
     md5hash = Digest::MD5.hexdigest(File.read filename_to_hash)
     log "hashed: (#{md5hash})"
     
     # Save the result in a variable
-    current_job.set_var(:md5hash, md5hash, self, current_action)
+    current_job.set_var(variable_to_set, md5hash, self, current_action)
 
     # Finished
     return 0, "md5 hash of (#{filename_to_hash}) is (#{md5hash})"
@@ -41,7 +45,7 @@ class StepMd5 < Step
   #private
   
   def validate_params?
-    return :path_variable unless self.pval(:path_variable).is_a? Hash
+    return :file_to_hash unless self.pval(:path).is_a? Hash
     return :result_variable unless self.pval(:result_variable).is_a? Hash
     return false
   end
